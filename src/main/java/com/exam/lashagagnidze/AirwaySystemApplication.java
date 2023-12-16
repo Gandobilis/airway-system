605a0e1edca3e350e1bd7d389763cb0125c4c3be
Gandobilis <lashadeveloper@gmail.com>
2023-12-16 21:33:31 +0400

    Initial commit

---
+++ src/main/java/com/exam/lashagagnidze/AirwaySystemApplication.java
@@ -0,0 +1,13 @@
+package com.exam.lashagagnidze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirwaySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirwaySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/lashagagnidze/AirwaySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.lashagagnidze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirwaySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
